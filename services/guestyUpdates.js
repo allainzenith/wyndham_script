498ff396618fc71f261bf856cce087a5c98378ce
allainzenith <allainzenith12@gmail.com>
2023-10-19 14:16:54 +0800

    216

--- services/guestyUpdates.js
+++ services/guestyUpdates.js
@@ -168,8 +168,6 @@ async function retrieveListings(substringAddress, token){
         const response = await axios.get(apiUrl, {headers : reqHeaders});
         const responseData = response.data;
 
-        console.log("response: ");
-        console.log(responseData);
         return responseData
 
     } catch (error) {
