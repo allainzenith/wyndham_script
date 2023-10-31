ea759c76992c05ef35d8471177f2998f0e78e95f
allainzenith <allainzenith12@gmail.com>
2023-10-31 11:45:19 +0800

    1145

--- nodeService.js
+++ nodeService.js
@@ -1,9 +1,9 @@
 const Service = require('node-windows').Service
 
 const svc = new Service({
-    name: "wyndhamScript",
+    name: "wyndhamService1.1",
     description: "This is a script for scraping the Wyndham site and uploading it on Guesty.",
-    // script: "C:\\Users\\Administrator\\Desktop\\mittenhousing-new\\bin\\www"
+    script: "C:\\Users\\unnamed89201\\Desktop\\Allain Documents\\OTHERS\\mittenhousing-new\\bin\\www"
 })
 
 svc.on('install', function(){
