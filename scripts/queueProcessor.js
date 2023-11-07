55eff30d74a2e5f934f6850823f71815de1052ca
allainzenith <allainzenith12@gmail.com>
2023-11-07 15:26:57 +0800

    326

--- scripts/queueProcessor.js
+++ scripts/queueProcessor.js
@@ -3,12 +3,13 @@ const { sharedData } = require("../config/puppeteerOptions");
 const { launchPuppeteer } = require("../services/scraper");
 
 const taskQueue = [];
+const scheduledtaskQueue = [];
 let isProcessing = false;
 let needtolaunchPuppeteer = true;
 
 async function processQueue() {
   if (isProcessing) return;
-  if (taskQueue.length === 0) {
+  if (taskQueue.length === 0 && scheduledtaskQueue.length === 0) {
     const browser = sharedData.browser;
     await browser.close();
     needtolaunchPuppeteer = true;
@@ -16,15 +17,29 @@ async function processQueue() {
   }
 
   isProcessing = true;
-  const { task, args, callback } = taskQueue.shift();
+  
+  if (taskQueue.length > 0) {
+    const { task, args, callback } = taskQueue.shift();
+  
+    // Execute the task (assuming it's a function)
+    task(...args, () => {
+      isProcessing = false;
+      processQueue();
+      callback();
+    });
+  } else if (scheduledtaskQueue.length > 0) {
+    console.log("No ongoing one listing tasks.");
+    const { task, args, callback } = scheduledtaskQueue.shift();
+  
+    task(...args, () => {
+      isProcessing = false;
+      processQueue();
+      callback();
+    });
+  }
 
-  // Execute the task (assuming it's a function)
-  task(...args, () => {
-    isProcessing = false;
-    processQueue();
-    callback();
-  });
 }
+
 
 async function addToQueue(task, callback, ...args) {
   if (needtolaunchPuppeteer) {
@@ -37,6 +52,20 @@ async function addToQueue(task, callback, ...args) {
     );
   }
   taskQueue.push({ task, args, callback });
+  await processQueue();
+}
+
+async function addToScheduledQueue(task, callback, ...args) {
+  if (needtolaunchPuppeteer) {
+    needtolaunchPuppeteer = false;
+    console.log("launching puppeteer now");
+    await launchPuppeteer();
+  } else {
+    console.log(
+      "puppeteer is already launched. execution of prior task is ongoing."
+    );
+  }
+  scheduledtaskQueue.push({ task, args, callback });
   await processQueue();
 }
 
@@ -78,4 +107,5 @@ async function resourceIntensiveTask(
 module.exports = {
   addToQueue,
   resourceIntensiveTask,
+  addToScheduledQueue
 };
