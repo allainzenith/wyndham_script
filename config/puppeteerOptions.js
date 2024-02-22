aff0b89bba9dd1b00f3976b98d5f9721e5b8321c
allainzenith <allainzenith12@gmail.com>
2024-02-22 10:23:36 +0800

    1023

--- config/puppeteerOptions.js
+++ config/puppeteerOptions.js
@@ -77,7 +77,7 @@ async function initializeBrowser(customProfileRelPath) {
       //   '--single-process',
       // ],
       // headless: 'new', 
-      // executablePath: 'C:\\Users\\Administrator\\chrome\\win64-122.0.6261.57\\chrome-win64\\chrome.exe',
+      executablePath: '/usr/bin/chromium-browser',
       headless: false,
       // headless: false,
       userDataDir: customProfileDir
