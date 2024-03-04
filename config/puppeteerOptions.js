ebec9455f24cb1102b2c6670ec131fd45abbc937
allainzenith <allainzenith12@gmail.com>
2024-03-04 15:35:35 +0800

    changed to non headless

--- config/puppeteerOptions.js
+++ config/puppeteerOptions.js
@@ -67,8 +67,8 @@ async function initializeBrowser(customProfileRelPath) {
         "--no-sandbox",
         "--no-zygote",
       ],
-      headless: 'new',
-      // headless: false,
+      // headless: 'new',
+      headless: false,
       userDataDir: customProfileDir
     });
 
