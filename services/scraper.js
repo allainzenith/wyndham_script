7ef664cb52e8e612a398da24abade3ed56471bf8
allainzenith <allainzenith12@gmail.com>
2024-06-19 14:27:38 +0800

    extended waiting for month option

--- services/scraper.js
+++ services/scraper.js
@@ -718,6 +718,8 @@ async function selectMonth(page, monthNow) {
 
   await monthSelector.click();
 
+  await page.waitForSelector('.react-datepicker__month-year-option', { visible: true });
+
   const curentMonth = await page.evaluate((monthSelector, monthNow) => {
     const months = document.querySelectorAll(monthSelector);
   
