057792670ccbc13b5cce8112e1eefdd24be3888e
allainzenith <allainzenith12@gmail.com>
2024-06-20 22:26:15 +0800

    1025

--- app.js
+++ app.js
@@ -40,9 +40,9 @@ app.use(async (req, res, next) => {
 
     await deleteOldManualUpdates();
     // await new Promise(resolve => setTimeout(resolve, 60000));
-    await scheduledUpdates("TIER 1");
+    // await scheduledUpdates("TIER 1");
     // await new Promise(resolve => setTimeout(resolve, 60000));
-    await scheduledUpdates("TIER 2");
+    // await scheduledUpdates("TIER 2");
     // await new Promise(resolve => setTimeout(resolve, 60000));
     // await scheduledUpdates("TIER 3");
   }
